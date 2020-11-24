24cd2f5ce5337a66564e6714ba966f16555d8c8f
sumin_030829 <sumin0829@kako.com>
2020-11-24 20:23:26 +0900

    complete resultactivity

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimvoterenoir/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimvoterenoir/ResultActivity.java
@@ -24,6 +24,16 @@ public class ResultActivity extends AppCompatActivity {
 
         int[] textIds={R.id.text1,R.id.text2,R.id.text3,R.id.text4,R.id.text5,R.id.text6,R.id.text7,R.id.text8,R.id.text9};
         int[] ratingIds={R.id.rating_bar1,R.id.rating_bar2,R.id.rating_bar3,R.id.rating_bar4,R.id.rating_bar5,R.id.rating_bar6,R.id.rating_bar7,R.id.rating_bar8,R.id.rating_bar9};
+        Integer imgFileNames[]={R.id.im1,R.id.im2,R.id.im3,R.id.im4,R.id.im5,R.id.im6,R.id.im7,R.id.im8,R.id.im9};
+
+        int max=0;
+        int maxIndex=0;
+        for(int i=0;i<imgNames.length;i++){
+            if(voteCount[i]>max){
+                max=voteCount[i];
+            }
+        }
+
         for(int i=0;i<imgNames.length;i++){
             textVs[i]=findViewById(textIds[i]);
             ratingBars[i]=findViewById(ratingIds[i]);
@@ -32,6 +42,7 @@ public class ResultActivity extends AppCompatActivity {
             ratingBars[i].setRating((float)voteCount[i]);
         }
         Button btnBack=findViewById(R.id.btn_back);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
